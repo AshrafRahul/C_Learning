af3c2e1bf95b4e82d3b2048b034992d1f952455c
AshrafRahul <m.ashraful.islam007@gmail.com>
2023-01-13 16:17:02 +0600

    C || basic

---
+++ 1.Basic/4.EscapeSecuence.c
@@ -0,0 +1,13 @@
+/*
+Write a program that print your name
+University name
+phone number
+*/
+#include<stdio.h>
+int main()
+{
+    printf("\aAshraful Islam Rahul\n");//using escape sequence
+    printf("\nDIU\n");
+    getch();
+}
+
