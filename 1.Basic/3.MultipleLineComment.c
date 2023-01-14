a274904efabb9e5c165f125aba918f3b689e68f6
AshrafRahul <m.ashraful.islam007@gmail.com>
2023-01-14 10:22:23 +0600

    C || basic

---
+++ 1.Basic/3.MultipleLineComment.c
@@ -0,0 +1,12 @@
+/*
+Md Ashraful Islam
+Daffodil International University
+*/
+#include<stdio.h>
+int main()
+{
+    printf("Ashraful Islam Rahul\n");
+    printf("1234567489\n");
+    return 0;
+}
+
